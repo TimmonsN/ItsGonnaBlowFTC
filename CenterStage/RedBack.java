326d2f56bbcc92045d835fbab273bf0b722a7be2
NTimmons9 <63173256+NTimmons9@users.noreply.github.com>
2024-01-14 14:17:56 -0600

    Meet 3 Code Update

--- CenterStage/RedBack.java
+++ CenterStage/RedBack.java
@@ -13,22 +13,23 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.JavaUtil;
+import java.util.List;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
-import org.opencv.imgproc.Imgproc;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvInternalCamera;
-import org.openftc.easyopencv.OpenCvPipeline;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 @Autonomous
-
 
 public class RedBack extends LinearOpMode{
   private DcMotor frontLeft;
@@ -37,38 +38,47 @@ public class RedBack extends LinearOpMode{
   private DcMotor backLeft;
   private DcMotor slideLeft;
   private DcMotor slideRight;
-  private DcMotor intake;
   private DcMotor fly;
   private Servo bucket;
   private Servo plane;
-  private Servo intakePush;
+  private Servo clawOpen;
+  private Servo clawLift;
   private BNO055IMU RevIMUAsBNO055IMU;
+  private TfodProcessor tfod;
+  private VisionPortal visionPortal;
+  private int sensedPosition = 2;
+  private static final String TFOD_MODEL_FILE = "red2.tflite";
+    // Define the labels recognized in the model for TFOD (must be in training order!)
+  private static final String[] LABELS = {
+    "prop",
+  };
   
+  private static final double clawClosed = 0.54;
 
     @Override
     public void runOpMode()
     {
-        frontLeft = hardwareMap.get(DcMotor.class, "frontleft");
-        backRight = hardwareMap.get(DcMotor.class, "backright");
-        frontRight = hardwareMap.get(DcMotor.class, "frontright");
-        backLeft = hardwareMap.get(DcMotor.class, "backleft");
-        slideLeft = hardwareMap.get(DcMotor.class, "slideleft");
-        slideRight = hardwareMap.get(DcMotor.class, "slideright");
-        intake = hardwareMap.get(DcMotor.class, "intake");
-        fly = hardwareMap.get(DcMotor.class, "fly");
-        intakePush = hardwareMap.get(Servo.class, "intakePush"); 
-        plane = hardwareMap.get(Servo.class, "plane"); 
-        bucket = hardwareMap.get(Servo.class, "bucket"); 
+        frontLeft = hardwareMap.get(DcMotor.class, "LeftFront");
+        backRight = hardwareMap.get(DcMotor.class, "RightBack");
+        frontRight = hardwareMap.get(DcMotor.class, "RightFront");
+        backLeft = hardwareMap.get(DcMotor.class, "LeftBack");
+        slideLeft = hardwareMap.get(DcMotor.class, "SlideLeft");
+        slideRight = hardwareMap.get(DcMotor.class, "SlideRight");
+        fly = hardwareMap.get(DcMotor.class, "Airplane");
+        clawOpen = hardwareMap.get(Servo.class, "Claw"); 
+        clawLift = hardwareMap.get(Servo.class, "ClawUp"); 
+        plane = hardwareMap.get(Servo.class, "PlaneArm"); 
+        bucket = hardwareMap.get(Servo.class, "Bucket"); 
         RevIMUAsBNO055IMU = hardwareMap.get(BNO055IMU.class, "imu");
         slideLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         slideRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        slideLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
         slideLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         slideRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);  
@@ -89,65 +99,134 @@ public class RedBack extends LinearOpMode{
         IMUParameters.loggingEnabled = false;
         // Initialize IMU.
         RevIMUAsBNO055IMU.initialize(IMUParameters);
+        
+        slideLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        slideRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+       //initializing TFOD prior to the waitForStart() call
+        initTfod();
+        telemetryTfod();
+        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
+        telemetry.addData(">", "Touch Play to start OpMode");
+        telemetry.addData("Hi :)", sensedPosition);
+        telemetry.update();
+        
         //find pos
-        intakePush.setPosition();
-///*
-        if(//left){
-            strafe(.5, -7.5);
-            move(.75, -24);
-            strafe(.5,-24);
-            linePlace();
-            boardPlace(-24);
-        }
-        else if(//center){
-            strafe(.5, -7.5);
-            rotate(90);
-            move(.5, 24);
-            linePlace();
+//START
+        waitForStart();
+        telemetryTfod();
+        telemetry.addData("Hi :)", sensedPosition);
+        telemetry.update();
+        bucket.setPosition(.5);
+        clawLift.setPosition(.69);
+        clawOpen.setPosition(clawClosed - 0.01);
+       // move(0.5, 15);
+       
+      if (sensedPosition == 3) { //left (drives right)
+            strafe(.5, -5);
             rotate(180);
-            boardPlace(-40);
-        }
-        else if(//right){
-            strafe(.5, 48);
+            move(.75, -25);
+            strafe(.5, -27);
+            rotate(180);
             linePlace();
-            boardPlace(-48);
+            boardPlace(-23);
+            strafe(.5, -25);
         }
+        
+        else if (sensedPosition == 2) { //center (drives center, duh)
+            strafe(.5, -7.5);
+            move(0.25, 2);
+            rotate(90);
+            move(.75, 20);
+            rotate(90);
+            linePlace();
+            move(.5,-2);
+            rotate(180);
+            strafe(.5,-7);
+            boardPlace(-40);
+            move(0.25, 1);
+            strafe(.5, -23);
+        }
+        
+        
+        else {                      //right (drives left)
+            strafe(.5, -40);
+            move(.5, -2);
+            rotate(180);
+            linePlace();
+            boardPlace(-50);
+            move(0.25, 1);
+            rotate(180);
+            strafe(.5, -31);
+        }
+
+
+           
+        //}
 //*/
     }
 
 //Game Specific Methods
     public void linePlace(){
-        intake.setPower(-.25);
+        move(0.25, -1);
+        clawLift.setPosition(0.45);
+        sleep(750);
+        clawOpen.setPosition(clawClosed + 0.2);
+        sleep(750);
+        clawOpen.setPosition(clawClosed);
+        telemetry.update();
+        sleep(750);
+        clawLift.setPosition(0.69);
         sleep(1000);
-        intake.setPower(0);
+        move(0.25, 1);
+        telemetry.update();
     }
 
     public void boardPlace(int dist){
         //tick count of up pos
-        int x = 1800;
-        slideLeft.setPower(0);
-        slideRight.setPower(0);
+        int heightTotal = -1600;
+        int height = -900;
+        telemetry.addData("right slide", slideRight.getPower());
+        telemetry.addData("left slide", slideLeft.getPower());
+        telemetry.addData("left pos", slideLeft.getCurrentPosition());
+        telemetry.update();
         slideLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         slideRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        slideLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        slideRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        slideLeft.setTargetPosition(x);
-        slideRight.setTargetPosition(x);
-        slideLeft.setPower(.5);
-        slideRight.setPower(.5);
+        slideLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        slideRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         move(.25, dist);
-        while (slideLeft.isBusy()) { 
+        rotate(180);
+        slideLeft.setPower(-.8);
+        slideRight.setPower(-.8);
+        while (slideLeft.getCurrentPosition() > height) { 
           sleep(1);
+          telemetry.update();
         }
         slideLeft.setPower(0);
         slideRight.setPower(0);
+        bucket.setPosition(0.4);
+        sleep(500);
+        telemetry.update();
+        slideLeft.setPower(-.8);
+        slideRight.setPower(-.8);
+         while (slideLeft.getCurrentPosition() > heightTotal) { 
+          sleep(1);
+          telemetry.update();
+        }
         //find dump pos
-        bucket.setPosition();
+        slideLeft.setPower(0);
+        slideRight.setPower(0);
+        bucket.setPosition(.76);
         sleep(2000);
         //find up pos
-        bucket.setPosition();
-        strafe(.5, 24);
+        bucket.setPosition(.53);
+        sleep(1500);
+        slideLeft.setPower(0.8);
+        slideRight.setPower(0.8);
+        sleep(450);
+        slideLeft.setPower(0);
+        slideRight.setPower(0);
+        strafe(.5, -3);
 
     }
 
@@ -163,6 +242,7 @@ public class RedBack extends LinearOpMode{
         double power = 1;
         int direction;
         int x = 0;
+        int count = 0;
         if(distance >= 0){
             direction = 1;
         } else{
@@ -197,7 +277,7 @@ public class RedBack extends LinearOpMode{
           telemetry.addData("Stuck", "");
           telemetry.update();
         }
-        while (backLeft.isBusy() && x < 150) {
+        while (backLeft.isBusy() && count < 150) {
            if ((backLeft.getCurrentPosition() * direction) <= start) {
                 power = (Math.abs((backLeft.getCurrentPosition())) * (speed/start));
                 power += .1;
@@ -216,9 +296,9 @@ public class RedBack extends LinearOpMode{
             backLeft.setPower(power * direction);
             backRight.setPower(power * direction);
             }
-            telemetry.addData("power", x);
+            telemetry.addData("power", count);
             telemetry.update();
-            x++;
+            count++;
         }
         frontRight.setPower(0);
         frontLeft.setPower(0);
@@ -361,5 +441,96 @@ public class RedBack extends LinearOpMode{
         backRight.setPower(0);
         telemetry.addData("strafe", x);
         telemetry.update();
+    }
+    
+    //Vision sensing methods:
+    private void initTfod() {
+
+        // Create the TensorFlow processor by using a builder.
+        tfod = new TfodProcessor.Builder()
+
+            // With the following lines commented out, the default TfodProcessor Builder
+            // will load the default model for the season. To define a custom model to load, 
+            // choose one of the following:
+            //   Use setModelAssetName() if the custom TF Model is built in as an asset (AS only).
+            //   Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
+            //.setModelAssetName(TFOD_MODEL_ASSET)
+            .setModelFileName(TFOD_MODEL_FILE)
+
+            // The following default settings are available to un-comment and edit as needed to 
+            // set parameters for custom models.
+            //.setModelLabels(LABELS)
+            //.setIsModelTensorFlow2(true)
+            //.setIsModelQuantized(true)
+            //.setModelInputSize(300)
+            //.setModelAspectRatio(16.0 / 9.0)
+
+            .build();
+
+        // Create the vision portal by using a builder.
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+        // Set the camera (webcam vs. built-in RC phone camera).
+        builder.setCamera(BuiltinCameraDirection.BACK);
+
+        // Choose a camera resolution. Not all cameras support all resolutions.
+        //builder.setCameraResolution(new Size(640, 480));
+
+        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
+        //builder.enableLiveView(true);
+
+        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
+        //builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
+
+        // Choose whether or not LiveView stops if no processors are enabled.
+        // If set "true", monitor shows solid orange screen if no processors enabled.
+        // If set "false", monitor shows camera view without annotations.
+        //builder.setAutoStopLiveView(false);
+
+        // Set and enable the processor.
+        builder.addProcessor(tfod);
+
+        // Build the Vision Portal, using the above settings.
+        visionPortal = builder.build();
+
+        // Set confidence threshold for TFOD recognitions, at any time.
+        //tfod.setMinResultConfidence(0.75f);
+
+        // Disable or re-enable the TFOD processor at any time.
+        //visionPortal.setProcessorEnabled(tfod, true);
+
+    }   // end method initTfod()
+   
+   private void telemetryTfod() {
+        double bestX = 300;
+        double bestY = 0;
+        double mostConfident = 0;
+        List<Recognition> currentRecognitions = tfod.getRecognitions();
+        telemetry.addData("# Objects Detected", currentRecognitions.size());
+
+        // Step through the list of recognitions and display info for each one.
+        for (Recognition recognition : currentRecognitions) {
+            double x = (recognition.getLeft() + recognition.getRight()) / 2 ;
+            double y = (recognition.getTop()  + recognition.getBottom()) / 2 ;
+            if (recognition.getConfidence() > 0) {
+                mostConfident = recognition.getConfidence();
+                bestX = x;
+                bestY = y;
+            }
+            telemetry.addData(""," ");
+            telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
+            telemetry.addData("- Position", "%.0f / %.0f", x, y);
+            telemetry.addData("- Size", "%.0f x %.0f", recognition.getWidth(), recognition.getHeight());
+        }   // end for() loop
+        if (bestX < 213) {
+            sensedPosition = 3;
+        }
+        else if (bestX > 440) {
+            sensedPosition = 1;
+        }
+        else {
+            sensedPosition = 2;
+        }
+
     }
 }  
--- CenterStage/TeleOp.java
+++ CenterStage/TeleOp.java
@@ -11,7 +11,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
-public class Tele extends LinearOpMode {
+public class TeleTest extends LinearOpMode {
     
   private DcMotor frontLeft;
   private DcMotor backRight;
@@ -19,11 +19,12 @@ public class Tele extends LinearOpMode {
   private DcMotor backLeft;
   private DcMotor slideLeft;
   private DcMotor slideRight;
-  private DcMotor intake;
   private DcMotor fly;
+
   private Servo bucket;
   private Servo plane;
-  private Servo intakePush;
+  private Servo clawOpen;
+  private Servo clawLift;
   private BNO055IMU RevIMUAsBNO055IMU;
   private ElapsedTime runtime = new ElapsedTime();
   private ElapsedTime colorWait = new ElapsedTime();
@@ -31,63 +32,41 @@ public class Tele extends LinearOpMode {
   @Override
   public void runOpMode(){
     
-        frontLeft = hardwareMap.get(DcMotor.class, "frontleft");
-        backRight = hardwareMap.get(DcMotor.class, "backright");
-        frontRight = hardwareMap.get(DcMotor.class, "frontright");
-        backLeft = hardwareMap.get(DcMotor.class, "backleft");
-        slideLeft = hardwareMap.get(DcMotor.class, "slideleft");
-        slideRight = hardwareMap.get(DcMotor.class, "slideright");
-        intake = hardwareMap.get(DcMotor.class, "intake");
-        fly = hardwareMap.get(DcMotor.class, "fly");
-        intakePush = hardwareMap.get(Servo.class, "intakepush"); 
-        plane = hardwareMap.get(Servo.class, "plane"); 
-        bucket = hardwareMap.get(Servo.class, "bucket"); 
+        frontLeft = hardwareMap.get(DcMotor.class, "LeftFront");
+        backRight = hardwareMap.get(DcMotor.class, "RightBack");
+        frontRight = hardwareMap.get(DcMotor.class, "RightFront");
+        backLeft = hardwareMap.get(DcMotor.class, "LeftBack");
+        slideLeft = hardwareMap.get(DcMotor.class, "SlideLeft");
+        slideRight = hardwareMap.get(DcMotor.class, "SlideRight");
+        fly = hardwareMap.get(DcMotor.class, "Airplane");
+        clawOpen = hardwareMap.get(Servo.class, "Claw"); 
+        clawLift = hardwareMap.get(Servo.class, "ClawUp");
+        plane = hardwareMap.get(Servo.class, "PlaneArm"); 
+        bucket = hardwareMap.get(Servo.class, "Bucket"); 
         RevIMUAsBNO055IMU = hardwareMap.get(BNO055IMU.class, "imu");
-        slideLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        slideLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); 
         slideRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        slideLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        fly.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        slideLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        slideRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);  
-        slideLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        slideRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // slideLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        // slideRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);  
+        // slideLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // slideRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         
        // int minimum = slideLeft.getCurrentPosition();
 
 //VARIABLES:
     
-    /* int armCounter1 = 0;
-    int armCounter2 = 0;
-    int armCounter3 = 0;
-    int armCounter4 = 0;
-    // No boolean inPos0 = false;
-    boolean inPos1 = false;
-    boolean inPos2 = false;
-    boolean inPos3 = false;
-    double tick = 0;
-    double diff = 0;
-    double pwr = 0; */
-    int low = 1040;   // Testing encoder heights: 1040 ish, approx. 80 per inch
-    int mid = 1840;   //                          1840 ish
-    int high = 2640;  //                          2640 ish
-    int bottom = 0;   //                           240 ish
-   
-   
-    int front = 0;    // Encoder Position over guide
-    int back = -700;     // Encoder Position over gears
-    int top = -400;      // Encoder Position when straight up
-    int count1 = 0;
-    int count2 = 0;
-    boolean forward = false;
-    boolean backward = false;
-    double manualServo = 0;
-    boolean controlled = false;
+    double manualBucket = bucket.getPosition();
+    double manualGrab = clawOpen.getPosition();
+    double manualClaw = clawLift.getPosition();
 
     waitForStart();
 
@@ -98,28 +77,29 @@ public class Tele extends LinearOpMode {
 
       while (opModeIsActive()) {
 //Slow mode (and normal):
-        if(!gamepad1.left_bumper && !gamepad1.right_bumper){
-            frontLeft.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x) - gamepad1.right_stick_x * 0.75);
-            frontRight.setPower(gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x * 0.75);
-            backRight.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x) + gamepad1.right_stick_x * 0.75);
-            backLeft.setPower((gamepad1.left_stick_y + gamepad1.left_stick_x) - gamepad1.right_stick_x * 0.75);
-        }
-        else if(gamepad1.left_bumper || gamepad1.right_bumper){
+        if(gamepad1.left_bumper || gamepad1.right_bumper){
             frontLeft.setPower(((gamepad1.left_stick_y - gamepad1.left_stick_x) - gamepad1.right_stick_x) * 0.3);
             frontRight.setPower((gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x) * 0.3);
             backRight.setPower(((gamepad1.left_stick_y - gamepad1.left_stick_x) + gamepad1.right_stick_x) * 0.3);
             backLeft.setPower(((gamepad1.left_stick_y + gamepad1.left_stick_x) - gamepad1.right_stick_x) * 0.3);
         }
+        else {
+          frontLeft.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x) - gamepad1.right_stick_x * 0.75);
+          frontRight.setPower(gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x * 0.75);
+          backRight.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x) + gamepad1.right_stick_x * 0.75);
+          backLeft.setPower((gamepad1.left_stick_y + gamepad1.left_stick_x) - gamepad1.right_stick_x * 0.75);
+        }
 //slides always have same power
-      slideRight.setPower(slideLeft.getPower());
         
 //manual sliding
-      if (!gamepad2.x) {
-        slideLeft.setPower((1 * gamepad2.left_stick_y));
-      }
-      else if (gamepad2.x) {
-        slideLeft.setPower((0.33 * (gamepad2.left_stick_y)));
-      }
+  //    if (!gamepad2.x) {
+        slideLeft.setPower((.7 * gamepad2.left_stick_y));
+        slideRight.setPower((.7 * gamepad2.left_stick_y));
+   //   }
+   //   else if (gamepad2.x) {
+  //      slideLeft.setPower((.9 * (gamepad2.left_stick_y)));
+   //     slideRight.setPower((.9 * (gamepad2.left_stick_y)));
+  //    }
     // if (slideLeft.getCurrentPosition() < minimum) {
       // slideLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
        //slideLeft.setTargetPosition(minimum);
@@ -128,15 +108,41 @@ public class Tele extends LinearOpMode {
      
 //telemetry
         telemetry.addData("slideLeft pos: ", slideLeft.getCurrentPosition());
-        telemetry.addData("slideLeft pow: ", slideLeft.getPower());
+      //  telemetry.addData("slideLeft pow: ", slideLeft.getPower());
+        telemetry.addData("slide power:", slideLeft.getPower());
         telemetry.addData("bucket position:", bucket.getPosition());
-        telemetry.addData("right stick:", gamepad2.right_stick_y);
+        telemetry.addData("claw (open):", clawOpen.getPosition());
+        telemetry.addData("claw (lift):", clawLift.getPosition());
+        telemetry.addData("plane arm:", plane.getPosition());
+      //  telemetry.addData("right stick:", gamepad2.right_stick_y);
         telemetry.update();
 
 //Intake:
-        //manual intake
-        intake.setPower(gamepad2.right_stick_y * 1);
-
+        //manual intake claw up/down
+        if(((gamepad2.right_stick_y < 0) && (manualClaw <= 1)) && (clawLift.getPosition() <= 1)) {
+          manualClaw += 0.003;
+          clawLift.setPosition(manualClaw);
+        }
+        else if(((gamepad2.right_stick_y > 0) && (manualClaw >= 0)) && (clawLift.getPosition() >= 0)) {
+          manualClaw -= 0.003;
+          clawLift.setPosition(manualClaw);
+        }
+        if(gamepad2.dpad_down) {
+          manualClaw = 0.3;
+          clawLift.setPosition(0.3);
+        }
+        
+        
+        if(((gamepad2.right_bumper) && (manualGrab <= 1)) && (clawOpen.getPosition() <= 1)) {
+          manualGrab += 0.001;
+          clawOpen.setPosition(manualGrab);
+        }
+        else if(((gamepad2.left_bumper) && (manualGrab >= 0)) && (clawOpen.getPosition() >= 0)) {
+          manualGrab -= 0.001;
+          clawOpen.setPosition(manualGrab);
+        }
+        
+        
         /*
         //for button intake
         if (gamepad2.a){intake.setPower(1);}
@@ -144,81 +150,41 @@ public class Tele extends LinearOpMode {
         */
         
 //Bucket:
-        if(gamepad2.right_bumper){
+        if(gamepad1.a){
             //pos needs to be found
-            //bucket.setPosition();
+            bucket.setPosition(0.52);
+            manualBucket = 0.52;
         }
-        else if(gamepad2.left_bumper){
+        else if(gamepad1.b){
             //pos needs to be found
-            //bucket.setPosition();
+            manualBucket = 0.8;
+            bucket.setPosition(0.8);
         }
         
-        if(((gamepad2.right_trigger > 0.5) && (manualServo <= 1)) && (bucket.getPosition() <= 1)) {
-          manualServo += 0.01;
-          bucket.setPosition(manualServo);
+        if(((gamepad1.right_trigger > 0.5) && (manualBucket <= 0.81)) && (bucket.getPosition() <= 1)) {
+          manualBucket += 0.005;
+          bucket.setPosition(manualBucket);
         }
-        if(((gamepad2.left_trigger > 0.5) && (manualServo >= 0)) && (bucket.getPosition() >= 0)) {
-          manualServo -= 0.01;
-          bucket.setPosition(manualServo);
+        else if(((gamepad1.left_trigger > 0.5) && (manualBucket >= 0.35)) && (bucket.getPosition() >= 0)) {
+          manualBucket -= 0.005;
+          bucket.setPosition(manualBucket);
         }
 
 //Airplane
-        if (gamepad2.y){fly.setPower(1);}
+        if (gamepad2.y){
+          fly.setPower(0.35);
+        }
         if (gamepad2.b){
-            //plane.setPosition();
-            fly.setPower(0);
+          plane.setPosition(0.15);
         }
+        if (gamepad2.x) {
+          fly.setPower(0);
+        }
+        if (gamepad2.a) { 
+          plane.setPosition(0.35);
+        }
+      
 
-        
-//AutoLevel (tm)
-        
-       /* if(gamepad2.dpad_left && (controlled == false)){
-          armCounter1++;
-          if(armCounter1 == 1){
-            slideLeft.setTargetPosition(low);
-            slideLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            slideLeft.setPower(0.3);
-          }
-        }
-        else if(!gamepad2.dpad_left) {
-          armCounter1 = 0;
-        }
-        
-        if(gamepad2.dpad_up && (controlled == false)){
-          armCounter2++;
-          if(armCounter2 == 1){ 
-            slideLeft.setTargetPosition(mid);
-            slideLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            slideLeft.setPower(0.3);
-          }
-        }
-        else if(!gamepad2.dpad_up) {
-          armCounter2 = 0;
-        }
-        
-        if(gamepad2.dpad_right && (controlled == false)){
-          armCounter3++;
-          if(armCounter3 == 1){ 
-            slideLeft.setTargetPosition(low);
-            slideLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            slideLeft.setPower(0.3);
-          }
-        }
-        else if(!gamepad2.dpad_right) {
-          armCounter3 = 0;
-        }
-        
-        if(gamepad2.dpad_down && (controlled == false)) {
-          armCounter4++;
-          if(armCounter4 == 1){ 
-            slideLeft.setTargetPosition(bottom);
-            slideLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            slideLeft.setPower(0.3);
-          }
-        }
-        else if(!gamepad2.dpad_down) {
-          armCounter4 = 0; 
-        }
         
        /* deccel relic? if(inPos){
           diff = Math.abs(arm.getCurrentPosition() - tick);
